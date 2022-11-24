93cd4318f57214d8b662514a2604065cf0f3fa97
yxu6fingers <116807583+yxu6fingers@users.noreply.github.com>
2022-11-24 19:24:02 +0700

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+    var description = new Typed('#description', {
+        strings: ['WhiteHat hacker', 'discord.js developer', 'Discord server designer', 'Open source projects', 'VexeraBot', 'Hercules', 'GiveawayBot', 'FlyingTuna'],
+        shuffle: true,
+        loop: true,
+        typeSpeed: 100,
+        backSpeed: 100,
+        cursorChar: '_',
+    })
+})
